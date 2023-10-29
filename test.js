0a99d6b7385a70301b624c240d8654cf55e60408
tboronenko <tboronenko@tboronenkos-MacBook-Air.local>
2023-10-29 18:22:17 +0200

    First commit

---
+++ test.js
@@ -0,0 +1,21 @@
+// Load the remote obj from WDIO package
+const { remote } = require("webdriverio");
+
+// Wrap our code in async. Now we can use await statments
+(async () => {
+  // Create new session using 'remote'. Saving the reference to a browser obj
+  const browser = await remote({
+    // port: 9515,
+    capabilities: {
+      browserName: "chrome",
+    },
+  });
+  // Send the URL command and ask the browser go to the WDIO website
+  await browser.url("https://tbpage.netlify.app");
+  // Get the title of the page and save it in locall variable
+  const title = await browser.getTitle();
+  console.log("Title was: " + title);
+  // Ending of our sesstion
+  await browser.deleteSession();
+  //   Simple catch the errors
+})().catch((e) => console.error(e));
