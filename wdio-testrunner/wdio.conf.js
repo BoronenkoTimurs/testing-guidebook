64c37d32eb9b704af9bde7ecab58249912cb4e1c
tboronenko <tboronenko@tboronenkos-MacBook-Air.local>
2023-11-10 15:28:42 +0200

    First test with Nikita support

--- wdio-testrunner/wdio.conf.js
+++ wdio-testrunner/wdio.conf.js
@@ -1,4 +1,4 @@
-console.log("Using wdio config file");
+// console.log("Using wdio config file");
 exports.config = {
   //
   // ====================
@@ -62,7 +62,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "info",
+  logLevel: "silent",
   //
   // Set specific log levels per logger
   // loggers:
@@ -86,7 +86,7 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: "2000",
+  baseUrl: "https://webdriver.io/",
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
@@ -102,7 +102,7 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ["gmail"],
+  services: ["chromedriver"],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
