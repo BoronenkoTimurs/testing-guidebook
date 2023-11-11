c64f6156c72f857c50c1517493f007fef6fea11e
tboronenko <tboronenko@tboronenkos-MacBook-Air.local>
2023-11-11 13:57:23 +0200

    Build docker containers

--- wdio-testrunner/test/specs/example.js
+++ wdio-testrunner/test/specs/example.js
@@ -1,11 +1,11 @@
 describe("webdriver.io page", () => {
   it("should have the right title", async () => {
-    browser.url("https://www.youtube.com/");
-    console.log("Using wdio config file");
-    console.log(browser.getTitle());
+    browser.url("https://webdriver.io/");
+    // console.log("Using wdio config file");
+    // console.log(browser.getTitle());
     await expect(browser).toHaveTitle(
       "WebdriverIO · Next-gen browser and mobile automation test \
-framework for Node.js"
+framework for Node.js | WebdriverIO"
     );
   });
 });
